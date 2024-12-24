066e2f724ee8088679b6cd99a5957ed352111a42
frederic moras <frederik.moras@vlaanderen.be>
2024-12-24 11:09:13 +0100

    fix(seed): correct class name casing in TestimonialSeeder
    Corrected the casing of `App\Models\Testimonial` to fix potential issues with model imports.

--- database/seeders/TestimonialSeeder.php
+++ database/seeders/TestimonialSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\testimonial;
+use App\Models\Testimonial;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
